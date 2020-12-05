93dfe982328db73aa3e74ba02bbc7a2592a68d7c
shradhasehgal <shradha.sgl@gmail.com>
2020-12-05 23:00:12 +0530

    Completed frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import * as d3 from 'd3';
 import counts from './counts_data.csv';
@@ -7,39 +6,85 @@ import hashtags from './hashtags_data.csv';
 
 class App extends Component {
 
-  constructor(props) {
-      super(props)
-  }
-  
-  componentDidMount() {
-  
-      d3.csv(counts)
-      .then(function(count) {
-          console.log(count)
-          d3.csv(hashtags)
-          .then(function(hashtag){
-              console.log(hashtag);
-              this.setState({count: count, hashtag: hashtag});
-          })
-          .catch(function(err){
-              throw err;
-          })
-      })
-      .catch(function(err) {
-          throw err;
-      })
-  }
-  
-  render() {
-  
-      return ( 
-               <div className = "App" >
-                <div> Data Visualization </div> 
-                {this.state.count.map((category, index) => (
-        <p>{category.name} from {person.country}!</p>))}
-               </div>
-          );
-      }
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: [],
+            hashtag: [],
+            category: ""
+        }
+        this.handleChangeCategory = this.handleChangeCategory.bind(this);
+    }
+
+    componentDidMount() {
+        var that = this;
+        // Load tweet count for hate classes
+        d3.csv(counts)
+            .then(function (countData) {
+                console.log(countData)
+                // Load hashtags for hate classes
+                d3.csv(hashtags)
+                    .then(function (hashtagData) {
+                        console.log(hashtagData);
+                        // Setting state with precomputed data from csv files
+                        that.setState({ count: countData, hashtag: hashtagData });
+                    })
+                    .catch(function (err) {
+                        throw err;
+                    })
+            })
+            .catch(function (err) {
+                throw err;
+            })
+    }
+
+    handleChangeCategory(event) {
+        this.setState({ category: event.target.value });
+    }
+
+    render() {
+
+        return (
+            <div className="App" >
+                <br />
+                <h1>Twitter Hate Speech</h1>
+                <form>
+                    <div class="form-group assign">
+                        <select value={this.state.category} class="form-control" onChange={this.handleChangeCategory}>
+                            <option disabled value="">Choose category</option>
+                            <option value="racism">Racism</option>
+                            <option value="sexism">Sexism</option>
+                            <option value="none">None</option>
+                        </select>
+                        <br />
+                    </div>
+                </form>
+                <br />
+
+                {this.state.count.map((obj, index) => {
+                    // Display count of tweets associated with chosen category
+                    return obj.category === this.state.category ?
+                        <div>
+                            <h2>Count of tweets belonging to class '{this.state.category}'</h2>
+                            <p>{obj.count}</p>
+                        </div>
+                        : ''
+                })}
+                {
+                    this.state.category !== "" ? <h2>Associated Hashtags</h2> : ''
+                }
+                {this.state.hashtag.map((obj, index) => {
+                    // Display hashtags associated with chosen category
+                    return obj.category === this.state.category ?
+                        <div>
+                            <p>#{obj.hashtag}</p>
+                        </div>
+                        : ''
+
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
