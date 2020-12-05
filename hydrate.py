de4413b2c7b0a6e6c7d061544f7fb2d5cabc7d63
shradhasehgal <shradha.sgl@gmail.com>
2020-12-05 17:11:40 +0530

    Tweet count added

--- hydrate.py
+++ hydrate.py
@@ -11,7 +11,9 @@ data_dir = "dataset"
 input_dataset = os.path.join(data_dir, "input_category.csv")
 final_dataset = os.path.join(data_dir, "tweets_data.csv")
 hashtags_dataset = os.path.join(data_dir, "hashtags_data.csv")
+counts_dataset = os.path.join(data_dir, "counts_data.csv")
 category_map = {}
+category_count = {"racism": 0, "sexism": 0, "none": 0}
 hashtags_count = {"racism": {}, "sexism": {}, "none": {}}
 
 def main():
@@ -25,12 +27,14 @@ def main():
             hydrate(path)
     
     hashtags_write(hashtags_dataset)
+    counts_write(counts_dataset)
 
 def hydrate(id_file):
     with open(final_dataset, 'w') as output:
         linewriter = csv.writer(output, delimiter=',', quotechar="\"")
         for tweet in twarc.hydrate(id_file.open()):
             category = category_map[tweet['id_str']]
+            category_count[category] += 1
             if "hashtags" in tweet["entities"]:
                 hashtags_list = tweet["entities"]["hashtags"]
                 for hashtag in hashtags_list:
@@ -52,6 +56,12 @@ def hashtags_write(id_file):
         for category,hashtags in hashtags_count.items():
             for hashtag,count in hashtags.items():
                 linewriter.writerow([hashtag, count, category]) 
+
+def counts_write(id_file):
+    with open(counts_dataset, 'w') as output:
+            linewriter = csv.writer(output, delimiter=',', quotechar="\"")
+            for category,count in category_count.items():
+                linewriter.writerow([category, count]) 
 
 if __name__ == "__main__":
     main()
